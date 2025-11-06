871ef15bc2d30a777dc2eb0a9d9e47215aab167b
Manuel <abraham2004hf@gmail.com>
2025-11-06 17:13:12 -0500

    Creación del LayoutNavbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="bg-gray-800 text-white p-4 flex gap-6">
+      <ul>
+        <NavLink to="/empleados">Empleados </NavLink>
+        <NavLink to="/horarios">Horarios</NavLink>
+      </ul>
+      <button className="ml-auto bg-red-500 px-3 py-1 rounded">Cerrar sesión</button>
+    </nav>
+  );
+};
 
 export default Navbar
--- src/Route/ConfigurationRoutes.jsx
+++ src/Route/ConfigurationRoutes.jsx
@@ -1,13 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 import InicioSesion from "../Pages/Autenticacion/InicioSesion";
 import RecuperarPassword from "../Pages/Autenticacion/RecuperarPassword";
+import Empleados from "../Pages/Empleados/Empleados";
+import Horario from "../Pages/Horario/Horario";
+import LayoutConNavbar from "../Route/LayoutConNavbar";
+
 
 const ConfigurationRoutes = () => {
   return (
     <Routes>
+      {/* Rutas sin navbar */}
       <Route path="/" element={<InicioSesion />} />
       <Route path="/recuperar" element={<RecuperarPassword />} />
-      <Route path="*" element={ <h1>NOT FOUND</h1>} />
+
+      {/* Rutas con navbar */}
+      <Route element={<LayoutConNavbar />}>
+        <Route path="/empleados" element={<Empleados />} />
+        <Route path="/horarios" element={<Horario />} />
+      </Route>
+
+      <Route path="*" element={<h1>NOT FOUND</h1>} />
     </Routes>
   );
 };
