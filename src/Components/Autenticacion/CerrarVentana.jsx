6d1af76658f85ee54811a5fcb69a97518c48674f
Manuel <abraham2004hf@gmail.com>
2025-12-07 21:41:08 -0500

    Implementacion del CerrarVentana

---
+++ src/Components/Autenticacion/CerrarVentana.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CerrarVentana = () => {
+  return (
+    <>
+        <h1 className='font-bold text-xl'>Correo enviado</h1>
+        <p className='mt-3 text-sm text-center'>Se ha enviado un correo con las indicaciones para recuperar su cuenta.</p>
+        <span className='mt-2 bg-amber-300/20 p-2 px-5 rounded-3xl text-xs'>Puede cerrar la pestaña</span>
+    </>
+  )
+}
+
+export default CerrarVentana
--- src/Pages/Autenticacion/RecuperarPassword.jsx
+++ src/Pages/Autenticacion/RecuperarPassword.jsx
@@ -1,69 +1,85 @@
-import React, { useState } from 'react'
-import PersonIcon from '@mui/icons-material/Person';
-import InputFormulario from '../../Components/Autenticacion/InputFormulario';
-import EmailIcon from '@mui/icons-material/Email';
-import BotonFormulario from '../../Components/Autenticacion/BotonFormulario';
-import { useNavigate } from 'react-router-dom';
-import LoaderScreen from '../../Components/Layouts/LoaderScreen';
+import React, { useState } from "react";
+import PersonIcon from "@mui/icons-material/Person";
+import InputFormulario from "../../Components/Autenticacion/InputFormulario";
+import EmailIcon from "@mui/icons-material/Email";
+import BotonFormulario from "../../Components/Autenticacion/BotonFormulario";
+import { useNavigate } from "react-router-dom";
+import LoaderScreen from "../../Components/Layouts/LoaderScreen";
+import CerrarVentana from "../../Components/Autenticacion/CerrarVentana";
 
 const RecuperarPassword = () => {
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(false); 
+  const navigate = useNavigate();
+  const [enviado, setEnviado] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const handleEnviarCorreo = async (e) => {
-        e.preventDefault(); 
+  const handleEnviarCorreo = async (e) => {
+    e.preventDefault();
 
-        setLoading(true);
+    setLoading(true);
 
-        // Simulación del envío de correo (usa tu fetch real aquí)
-        await new Promise(resolve => setTimeout(resolve, 2000));
+    // Simulación del envío de correo (usa tu fetch real aquí)
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
-        setLoading(false);
+    setLoading(false);
+    setEnviado(true);
+   
+  };
 
-        navigate("/cambiar-password");
-    }
+  return (
+    <>
+      {loading && <LoaderScreen></LoaderScreen>}
 
-    return (
-        <>
-            { loading && ( <LoaderScreen></LoaderScreen> ) }
-
-            <section className="h-screen flex justify-center items-center 
-                bg-gradient-to-br from-red-900 via-purple-900 to-indigo-900"> 
-                
-                {/* Tarjeta con Efecto Glassmorphism */}
-                <div className="bg-white/0 backdrop-blur-xl rounded-3xl shadow-2xl shadow-black/40 p-8 w-[350px] flex flex-col items-center border border-white/10">               
-                    <div className='mb-6 '>
-                        <div className='w-24 h-24 rounded-full bg-white/20 flex justify-center items-center shadow-inner'>
-                            <PersonIcon className="!text-white opacity-70" sx={{ fontSize: 70 }} />
-                        </div>
-                    </div>
-                    <div className="pb-8 text-center">
-                        <h2 className="text-white text-xl font-semibold tracking-wide mb-2">
-                            Recuperar Contraseña
-                        </h2>
-
-                        <p className="text-white/80 text-sm leading-relaxed px-2">
-                            Ingresa el correo asociado a tu cuenta y te enviaremos un enlace para 
-                            restablecer tu contraseña de forma segura.
-                        </p>
-                    </div>
-                    <form action="" className='space-y-6 w-full text-white'>
-                        <div>
-                            {/* Campo Email */}
-                            <InputFormulario
-                                Icon={EmailIcon} 
-                                type="email" 
-                                placeholder="Email" 
-                            />
-                    </div>
-                        <div className='pt-3'>
-                            <BotonFormulario valor={"ENVIAR CORREO"} operacion={handleEnviarCorreo}></BotonFormulario>
-                        </div>
-                    </form>            
+      <section
+        className="h-screen flex justify-center items-center 
+                bg-gradient-to-br from-red-900 via-purple-900 to-indigo-900"
+      >
+        {/* Tarjeta con Efecto Glassmorphism */}
+        <div className="text-white bg-white/0 backdrop-blur-xl rounded-3xl shadow-2xl shadow-black/40 p-8 w-[350px] flex flex-col items-center border border-white/10">
+          {enviado ? (
+            <CerrarVentana />
+          ) : (
+            <>
+              <div className="mb-6 ">
+                <div className="w-24 h-24 rounded-full bg-white/20 flex justify-center items-center shadow-inner">
+                  <PersonIcon
+                    className="!text-white opacity-70"
+                    sx={{ fontSize: 70 }}
+                  />
                 </div>
-            </section>
-        </>
-  )
-}
+              </div>
 
-export default RecuperarPassword
+              <div className="pb-8 text-center">
+                <h2 className="text-white text-xl font-semibold tracking-wide mb-2">
+                  Recuperar Contraseña
+                </h2>
+
+                <p className="text-white/80 text-sm leading-relaxed px-2">
+                  Ingresa el correo asociado a tu cuenta y te enviaremos un
+                  enlace para restablecer tu contraseña de forma segura.
+                </p>
+              </div>
+
+              <form className="space-y-6 w-full text-white">
+                <div>
+                  <InputFormulario
+                    Icon={EmailIcon}
+                    type="email"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="pt-3">
+                  <BotonFormulario
+                    valor="ENVIAR CORREO"
+                    operacion={handleEnviarCorreo}
+                  />
+                </div>
+              </form>
+            </>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default RecuperarPassword;
